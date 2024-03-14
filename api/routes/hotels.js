9ddc292b8f532978605b91bed89fb9cd1e89e919
Pritam <pritammagdum24@gmail.com>
2024-03-14 15:39:08 +0530

    Added Controller for Update the Hotel Information

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,25 +1,14 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
-import { createHotel } from "../controllers/hotel.js";
+import { createHotel, updateHotel } from "../controllers/hotel.js";
 
 const router = express.Router();
 // create
 router.post("/", createHotel);
 
 // update
-router.put("/:id", async (req, res) => {
-  try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updatedHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put("/:id", updateHotel);
 
 // delete
 router.delete("/:id", async (req, res) => {
