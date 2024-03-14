cc0963d689b93b33620c1704202e48cb27763873
Pritam <pritammagdum24@gmail.com>
2024-03-14 17:54:36 +0530

    Added All User Endpoint Routes in User Controller

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,28 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("Hello User You are Logged In!");
+});
+
+// update
+router.put("/:id", updateUser);
+
+// delete
+router.delete("/:id", deleteUser);
+
+// get Hotel
+router.get("/:id", getUser);
+
+// get all
+router.get("/", getUsers);
 
 export default router;
