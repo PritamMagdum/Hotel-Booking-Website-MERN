7983d61385d2190e6b1e46c642f7201f3573ee80
Pritam <pritammagdum24@gmail.com>
2024-03-14 14:25:42 +0530

    Added Enpoint to Create Hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,25 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
+// create
+router.post("/", async (req, res) => {
+  // console.log("req is-->", req);
+  const newHotel = new Hotel(req.body);
+
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    // console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// update
+
+// delete
+// get
+// get all
 
 export default router;
