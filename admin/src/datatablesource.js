9daffdf5bde565e6baf6856490cb6155058916a1
Pritam <pritammagdum24@gmail.com>
2024-03-17 09:48:25 +0530

    Added Data Table Source

---
+++ admin/src/datatablesource.js
@@ -0,0 +1,89 @@
+export const userColumns = [
+  { field: "_id", headerName: "ID", width: 150 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 200,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img
+            className="cellImg"
+            src={params.row.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"}
+            alt="avatar"
+          />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 220,
+  },
+
+  {
+    field: "country",
+    headerName: "Country",
+    width: 100,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  },
+];
+
+export const hotelColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 180,
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 100,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 220,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+];
+
+export const roomColumns = [
+  { field: "_id", headerName: "ID", width: 250 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 150,
+  },
+  {
+    field: "desc",
+    headerName: "Description",
+    width: 250,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 100,
+  },
+  {
+    field: "maxPeople",
+    headerName: "Max People",
+    width: 100,
+  },
+];
